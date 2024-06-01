9dee1a6748aeb59106347d9ad9e0b9d06e08ebfe
leo <leoyunhyung@gmail.com>
2024-06-01 22:14:51 +0900

    refactor: update user field

--- cheonbaeksa/apps/users/admin.py
+++ cheonbaeksa/apps/users/admin.py
@@ -1,10 +1,6 @@
 # Django
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.html import format_html
-
-# Inlines
-from cheonbaeksa.apps.users.inlines import SocialAccountInline
 
 # Bases
 from cheonbaeksa.bases.admin import Admin
@@ -15,13 +11,13 @@ from cheonbaeksa.apps.users.models.index import User
 
 @admin.register(User)
 class UserAdmin(Admin, UserAdmin):
-    list_display = ('email', 'name', 'is_staff')
+    list_display = ('email', 'is_email_verified', 'is_staff')
     search_fields = ('email', 'name')
-    list_filter = ('is_staff',)
+    list_filter = ('is_staff', 'is_email_verified')
     ordering = ('-created',)
 
     fieldsets = (
-        ('1. 정보', {'fields': ('id', 'email', 'name', 'password', 'auth_token')}),
+        ('1. 정보', {'fields': ('id', 'email', 'is_email_verified', 'password', 'auth_token')}),
         ('2. 권한', {'fields': ('is_staff',)}),
         ('3. 생성일 / 수정일', {'fields': ('created', 'modified')}),
     )
@@ -31,5 +27,4 @@ class UserAdmin(Admin, UserAdmin):
         ('2. 권한', {'fields': ('is_staff',)}),
     )
 
-    readonly_fields = ('auth_token', "created", "modified")
-    inlines = (SocialAccountInline,)
+    readonly_fields = ('auth_token', 'created', 'modified', 'is_email_verified')
--- cheonbaeksa/apps/users/models/index.py
+++ cheonbaeksa/apps/users/models/index.py
@@ -6,11 +6,7 @@ from django.utils.translation import gettext_lazy as _
 # Bases
 from cheonbaeksa.bases.models import Model
 
-# Utils
-from cheonbaeksa.utils.validators import validator_name
-
 # Models
-from cheonbaeksa.apps.users.models.fields.phone_number import PhoneNumberField
 from cheonbaeksa.apps.users.models.managers.objects import UserMainManager
 
 
@@ -19,12 +15,7 @@ class User(AbstractUser,
            Model):
     # Main
     email = models.EmailField(_('이메일'), unique=True,
-                              error_messages={'unique': _('이미 있는 이메일입니다.')})
-    phone = PhoneNumberField(_('전화'), max_length=20, null=True, blank=True)
-    name = models.CharField(_('이름'), max_length=20, null=True, blank=True,
-                            validators=[validator_name])
-
-    # Info
+                              error_messages={'unique': _('이미 사용중인 이메일 입니다.')})
     is_email_verified = models.BooleanField(_('이메일 인증 여부'), default=False)
 
     USERNAME_FIELD = 'email'
