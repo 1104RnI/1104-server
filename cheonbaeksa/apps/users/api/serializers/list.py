ddd389e1a7f7c66b19afc50789240206b0d8e4ae
leo <leoyunhyung@gmail.com>
2024-06-03 16:17:25 +0900

    feat: add user list serializer

---
+++ cheonbaeksa/apps/users/api/serializers/list.py
@@ -0,0 +1,12 @@
+# Bases
+from cheonbaeksa.bases.api.serializers import ModelSerializer
+
+# Models
+from cheonbaeksa.apps.users.models.index import User
+
+
+# Main Section
+class UserListSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'is_email_verified')
