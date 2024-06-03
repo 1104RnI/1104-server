caaa7d95141071b57796f3f095cddc87bb27b3db
leo <leoyunhyung@gmail.com>
2024-06-03 18:07:47 +0900

    chore: resister user view mixins init

---
+++ cheonbaeksa/apps/users/api/views/mixins/__init__.py
@@ -0,0 +1,4 @@
+from cheonbaeksa.apps.users.api.views.mixins.login import UserLoginViewMixin
+from cheonbaeksa.apps.users.api.views.mixins.me import UserMeViewMixin
+from cheonbaeksa.apps.users.api.views.mixins.signup import UserSignupViewMixin
+from cheonbaeksa.apps.users.api.views.mixins.validate import UserValidateViewMixin
