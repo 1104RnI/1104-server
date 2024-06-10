00545fb51135d8e20936b296b8681602e45aa544
leo <leoyunhyung@gmail.com>
2024-06-10 19:28:29 +0900

    feat: update user permissions

--- cheonbaeksa/apps/users/api/views/permissions/index.py
+++ cheonbaeksa/apps/users/api/views/permissions/index.py
@@ -11,4 +11,9 @@ class UserPermission(BasePermission):
             'me', 'update_password', 'update_trading_view_username',
         ] and request.user.is_anonymous:
             return False
+
+        # 이메일 인증이 완료되지 않은 사용자는 접근을 제한합니다.
+        if request.user.is_authenticated and not request.user.is_email_verified:
+            return False
+
         return True
