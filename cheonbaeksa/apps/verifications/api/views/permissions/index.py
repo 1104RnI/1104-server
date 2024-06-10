b91a1d9a24ffdb545c7e71c9d1fc21265dfc208f
leo <leoyunhyung@gmail.com>
2024-06-11 01:02:32 +0900

    feat: update email verification permissions

--- cheonbaeksa/apps/verifications/api/views/permissions/index.py
+++ cheonbaeksa/apps/verifications/api/views/permissions/index.py
@@ -8,7 +8,7 @@ class EmailVerificationPermission(BasePermission):
     def has_permission(self, request, view):
         # 로그인하지 않은 사용자는 접근을 제한합니다.
         if view.action in [
-            'signup_email_verification',
+            'signup_email', 'signup_email_verify'
         ] and request.user.is_anonymous:
             return False
         return True
