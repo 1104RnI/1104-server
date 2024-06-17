aad5d6519a2ea249395f6fa011506efb032dc261
leo <leoyunhyung@gmail.com>
2024-06-17 14:37:39 +0900

    chore: remove user list API

--- cheonbaeksa/apps/users/api/serializers/__init__.py
+++ cheonbaeksa/apps/users/api/serializers/__init__.py
@@ -1,6 +1,5 @@
 from cheonbaeksa.apps.users.api.serializers.auth import UserLoginSuccessSerializer, UserLoginSerializer
 from cheonbaeksa.apps.users.api.serializers.create import UserSignupSerializer
-from cheonbaeksa.apps.users.api.serializers.list import UserListSerializer
 from cheonbaeksa.apps.users.api.serializers.retrieve import UserMeSerializer
 from cheonbaeksa.apps.users.api.serializers.token import CustomTokenRefreshSerializer
 from cheonbaeksa.apps.users.api.serializers.update import UserPasswordUpdateSerializer, \
