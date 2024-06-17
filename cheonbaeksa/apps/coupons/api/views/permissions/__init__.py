b734e08d699739590c150fd9c1bd1b44566ff8af
leo <leoyunhyung@gmail.com>
2024-06-17 14:32:46 +0900

    feat: add coupon permissions

---
+++ cheonbaeksa/apps/coupons/api/views/permissions/__init__.py
@@ -0,0 +1,1 @@
+from cheonbaeksa.apps.coupons.api.views.permissions.index import CouponPermission
---
+++ cheonbaeksa/apps/coupons/api/views/permissions/index.py
@@ -0,0 +1,15 @@
+# DRF
+from rest_framework.permissions import BasePermission
+
+
+# Main Section
+class CouponPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        # 로그인하지 않은 사용자는 접근을 제한합니다.
+        if view.action in [
+            'coupon_check',
+        ] and request.user.is_anonymous:
+            return False
+
+        return True
