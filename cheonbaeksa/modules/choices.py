9bd561c3988ea2585863978dbff6ecb182d8114c
leo <leoyunhyung@gmail.com>
2024-06-17 13:39:44 +0900

    style: comments

--- cheonbaeksa/modules/choices.py
+++ cheonbaeksa/modules/choices.py
@@ -1,6 +1,8 @@
+# Django
 from model_utils import Choices
 from django.utils.translation import gettext_lazy as _
 
+# Main Section
 PURPOSE_CHOICES = Choices(
     ('SIGNUP', _('회원가입')),
     ('PASSWORD_RESET', _('비밀번호 변경')),
