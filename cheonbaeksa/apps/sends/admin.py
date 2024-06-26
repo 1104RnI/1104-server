0d91d6a40dedb6180449e0c36e853fac42a07544
leo <leoyunhyung@gmail.com>
2024-06-26 15:43:55 +0900

    feat: update fields

--- cheonbaeksa/apps/sends/admin.py
+++ cheonbaeksa/apps/sends/admin.py
@@ -11,12 +11,12 @@ from cheonbaeksa.apps.sends.models import EmailSend
 # Main Section
 @admin.register(EmailSend)
 class EmailSendAdmin(Admin):
-    list_display = ('sender_email', 'recipient_email', 'title', 'purpose', 'status')
-    search_fields = ('sender_email', 'recipient_email', 'title')
+    list_display = ('from_email', 'to_email', 'title', 'purpose', 'status')
+    search_fields = ('from_email', 'to_email', 'title')
     list_filter = ('purpose', 'status')
 
     fieldsets = (
-        ('1. 정보', {'fields': ('sender_email', 'recipient_email', 'title', 'content', 'purpose')}),
+        ('1. 정보', {'fields': ('from_email', 'to_email', 'title', 'content', 'purpose')}),
         ('2. 상태', {'fields': ('status',)}),
         ('3. 활성화', {'fields': ('is_active',)}),
         ('4. 날짜', {'fields': ('created', 'modified')}),
--- cheonbaeksa/apps/sends/models/email.py
+++ cheonbaeksa/apps/sends/models/email.py
@@ -12,8 +12,8 @@ from cheonbaeksa.modules.choices import PURPOSE_CHOICES
 # Main Section
 class EmailSend(Model):
     # Main
-    sender_email = models.EmailField(verbose_name=_('보낸 사람 이메일'))
-    recipient_email = models.EmailField(verbose_name=_('받는 사람 이메일'))
+    from_email = models.EmailField(verbose_name=_('보낸 사람 이메일'))
+    to_email = models.EmailField(verbose_name=_('받는 사람 이메일'))
     title = models.CharField(verbose_name=_('타이틀'), max_length=100)
     content = models.TextField(verbose_name=_('본문'))
     purpose = models.CharField(verbose_name=_('이메일 전송 목적'), choices=PURPOSE_CHOICES, max_length=20)
